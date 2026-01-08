baa39af94e78660503b52abfeb4c31dfa50d5f78
Safal Acharya <acharyasafal51@gmail.com>
2026-01-08 14:14:38 +0545

    Add: adding helper in books

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
-module BooksHelper
+    module BooksHelper
+  def formatted_author(book)
+    if book.rating.present? && book.rating > 4
+      "Masterpiece by #{book.author}"
+    else
+      "by #{book.author}"
+    end
+  end
 end
+
