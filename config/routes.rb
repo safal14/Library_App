4ad76d7829fc5db23cd0c082d3db7470929177fc
Safal Acharya <acharyasafal51@gmail.com>
2026-01-08 12:09:13 +0545

    Fix: fixing a extra code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
- root "books#index"
 get "/books", to: "books#index"
   get '/books/new', to: 'books#new', as: :new_book
   get "books/:id", to: "books#show", as: :book
